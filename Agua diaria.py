26dd164461b9090542b2e40f6ca1ac9e4a473598
ErickCorreia <correiaerick18@sempreceub.com>
2025-05-10 22:06:29 -0300

    Add files via upload
    Código que visa calcular a média diária de água

---
+++ Agua diaria.py
@@ -0,0 +1,5 @@
+massa = float(input("Digite seu peso em kg: "))
+
+necessidade_agua = massa * 0.03  # Corrigindo o nome da variável
+
+print(f"Você deve beber {necessidade_agua:.2f} litros de água por dia.")
